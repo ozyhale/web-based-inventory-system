af65d18931c51f7bf86da1f380e669a509d7c601
kristofferkrisanore@yahoo.com <kristofferkrisanore@yahoo.com@960309bf-f6c2-cfab-c875-e90d0b094961>
2013-03-19 00:20:59 +0000

    sample commit for locally modified source code.

--- trunk2/Web-Based-Inventory-System/index.php
+++ trunk2/Web-Based-Inventory-System/index.php
@@ -10,6 +10,7 @@
         // so this is the svn client. subversion 3rd party in netbeans.
         // it has functionality of commiting the source code, updating , differ
         // sample output in comitiing..
+        //finished.. then go to cloud server to get and edit the source code anywhere,anytime ..
         ?>
     </body>
 </html>
